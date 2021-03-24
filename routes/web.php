a7a44ab65e90357b752c3a4d1ade4a61d0bc7878
EC2 Default User <ec2-user@ip-172-31-8-53.ap-northeast-1.compute.internal>
2021-03-24 23:16:31 +0000

    laravel応用課題11,12の提出

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,31 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', "UsersController@index");
 
 // ユーザ登録
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup');
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
+Route::get("login", "Auth\LoginController@showloginForm")->name("login");
+Route::post("login", "Auth\LoginController@login")->name("login.post");
+Route::get("logout", "Auth\LoginController@logout")->name("logout");
+
+Route::resource("users", "UsersController", ["only" => ["show"]]);
+
+Route::group(['prefix' => 'users/{id}'], function(){
+    Route::get('followings', 'UsersController@followings')->name('followings');
+    Route::get('followers', 'UsersController@followers')->name('followers');
+    });
+
+Route::group(["middleware" => "auth"], function(){
+    Route::put("users", "UsersController@rename")->name("rename");
+   
+    Route::group(["prefix" => "users/{id}"], function(){
+        Route::post("follow", "UserFollowController@store")->name("follow");
+        Route::delete("unfollow", "UserFollowController@destroy")->name("unfollow");
+    });
+   
+    Route::resource("movies", "MoviesController", ["only" => ["create", "store", "destroy"]]);
+    
+});
+
